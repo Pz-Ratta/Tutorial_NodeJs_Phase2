12497dd2434f7f3b608ce19da78f34341a98710f
chakkrapan <chakkrapan.r@amadothailand.co.th>
2024-09-26 17:16:19 +0700

    ttt

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -2,8 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    const name = "PPPPP";
-    res.render('index.ejs',{data:name})
+    const products = [
+        {name:"โน๊ตบุ๊ค", price: 22000, image:"images/products/product1.png"},
+        {name:"เสื้อ", price: 2000, image:"images/products/product2.png"},
+        {name:"หูฟัง", price: 200, image:"images/products/product3.png"}
+
+    ]
+    res.render('index', {products:products});
 });
+
+router.get('/addForm', (req, res)=>{
+    res.render('form');
+})
+
+router.get('/manage', (req, res)=>{
+    res.render('manage');
+})
+
+router.post('/insert', (req, res)=>{
+    console.log(req.body);
+    res.render('form');
+
+})
 
 module.exports = router;
