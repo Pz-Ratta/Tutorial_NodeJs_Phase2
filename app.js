ada9232e50cdd1e58e0569d83e4b1d5d92420e92
chakkrapan <chakkrapan.r@amadothailand.co.th>
2024-09-26 12:00:33 +0700

    1st commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes/myRouter');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(8080,()=>{
+    console.log('Start server on port 8080');
+});
--- modules.js
+++
@@ -1,5 +0,0 @@
-const mymodules = require('./modules/mymodules.js');
-
-const number = 5000000;
-
-console.log(mymodules.formatNumber(number));
